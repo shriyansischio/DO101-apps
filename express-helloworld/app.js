ccb3910ea1cbf05925a3ee6f504a186b1c348a56
shriyansischio <shriyansdenis@gmail.com>
2023-11-25 12:57:30 +0530

    add /baby endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/baby', function(req, res){
+  res.send("mars")
+
+});
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
